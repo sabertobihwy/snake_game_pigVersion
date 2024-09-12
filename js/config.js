d2a4ae3396b2d59a00628e80c3db238a0fc81f99
wenyan <sabertobihwy@gmail.com>
2024-09-11 21:33:16 -0400

    'move()'

--- js/config.js
+++ js/config.js
@@ -4,13 +4,42 @@ var rows = 20
 var cols = 20
 var snakebody = 20
 
+var dirs = {
+    up: {
+        x: 0,
+        y: -1,
+        rotate: 'rotate(-90deg)',
+        flag: 'up'
+    },
+    down: {
+        x: 0,
+        y: 1,
+        rotate: 'rotate(90deg)',
+        flag: 'down'
+    },
+    left: {
+        x: -1,
+        y: 0,
+        rotate: 'rotate(180deg)',
+        flag: 'left'
+    },
+    right: {
+        x: 1,
+        y: 0,
+        rotate: 'rotate(0deg)',
+        flag: 'right'
+    }
+}
 var snake = {
     snakePos: [
         { x: 0, y: 0, dom: "", flag: 'body' },
         { x: 1, y: 0, dom: "", flag: 'body' },
         { x: 2, y: 0, dom: "", flag: 'body' },
         { x: 3, y: 0, dom: "", flag: 'head' }
-    ]
+    ],
+    dir: dirs.right,
+    isCollide: false,
+    getFood: false
 
 }
 
--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
-function initGame() {
+function main() {
+    initMap()
+    drawSnake()
+    drawFood()
+}
+
+function initMap() {
     // 1. init map
     for (let i = 0; i < 20; i++) {
         for (let j = 0; j < 20; j++) {
@@ -8,6 +14,10 @@ function initGame() {
             })
         }
     }
+}
+
+
+function drawSnake() {
     // 2. init snake: create dom
     snake.snakePos.forEach((snk) => {
         if (!snk.dom) {
@@ -16,21 +26,27 @@ function initGame() {
             div.style.width = snakebody + 'px'
             div.style.height = snakebody + 'px'
             div.style.position = 'absolute'
-            if (snk.flag === 'body') {
-                div.style.backgroundColor = '#E2A1A1'
-                div.style.borderRadius = '50px'
-            } else {
-                div.style.background = 'url(../img/pig.png) center/contain no-repeat'
-            }
             snk.dom = div
         }
+        if (snk.flag === 'body') {
+            snk.dom.style.backgroundImage = 'none'
+            snk.dom.style.backgroundColor = '#E2A1A1'
+            snk.dom.style.borderRadius = '50px'
+        } else {
+            snk.dom.style.background = 'url(../img/pig.png) center/contain no-repeat'
+            snk.dom.style.transform = snake.dir.rotate
+        }
+
         // change position 
         snk.dom.style.left = snk.x * snakebody + 'px'
         snk.dom.style.top = snk.y * snakebody + 'px'
         // 3. draw snake
         container.appendChild(snk.dom)
     })
+}
 
+
+function drawFood() {
     // initfood 
     // find valid food position 
     isValid = true
@@ -39,7 +55,7 @@ function initGame() {
         var foodY = Math.floor(Math.random() * 20)
         snake.snakePos.forEach((snk) => {
             if (snk.x === foodX && snk.y === foodY) {
-                isValid = false;
+                isValid = false; // forEach不可以break
             }
         })
         if (isValid) {
@@ -58,14 +74,92 @@ function initGame() {
         div.style.width = snakebody + 'px'
         div.style.height = snakebody + 'px'
         div.style.position = 'absolute'
-        div.style.background = `url(../img/food/meat-${index}.png) center/contain no-repeat`
         foodPos.dom = div
     }
+    foodPos.dom.style.background = `url(../img/food/meat-${index}.png) center/contain no-repeat`
     foodPos.dom.style.left = foodPos.x * snakebody + 'px'
     foodPos.dom.style.top = foodPos.y * snakebody + 'px'
     container.appendChild(foodPos.dom)
     console.log(foodPos.dom)
+}
 
+function isCollide() {
+    // 1. edge 
+    var oldHead = snake.snakePos[snake.snakePos.length - 1]
+    var newHead = {
+        x: oldHead.x + snake.dir.x,
+        y: oldHead.y + snake.dir.y,
+        dom: "",
+        flag: 'head'
+    }
+    if (((newHead.x < 0) || (newHead.x >= 20)) ||
+        ((newHead.y < 0) || (newHead.y >= 20))) {
+        snake.isCollide = true;
+        return
+    }
+    // 2. snake itself 
+    snake.snakePos.forEach((p) => {
+        if (newHead.x === p.x && newHead.y === p.y) {
+            snake.isCollide = true;
+        }
+    })
+
+    // 3. getFood?
+    if (newHead.x === foodPos.x && newHead.y === foodPos.y) {
+        snake.getFood = true
+    }
+    return newHead
+}
+
+function moveSnake() {
+    // 1. iscollide?
+    var newHead = isCollide()
+
+    // collide -> return 
+    // getfood -> change food pos 
+    if (snake.isCollide) {
+        console.log('collide')
+        return
+    }
+
+    if (snake.getFood) {
+        drawFood()
+    }
+
+    // 2. change snake pos 
+    // 2.1 body
+    snake.snakePos[snake.snakePos.length - 1].flag = 'body'
+    var tail = snake.snakePos.shift() // remove the tail
+    container.removeChild(tail.dom)
+    // 2.2  head 
+    snake.snakePos.push(newHead)
+
+    // 3. draw the dom 
+    drawSnake()
 
 }
-initGame()
+
+document.onkeydown = function (e) {
+    switch (e.key) {
+        case 'ArrowUp':
+            snake.dir = dirs.up
+            break
+        case 'ArrowDown':
+            snake.dir = dirs.down
+            break
+        case 'ArrowLeft':
+            snake.dir = dirs.left
+            break
+        case 'ArrowRight':
+            snake.dir = dirs.right
+            break
+    }
+    moveSnake()
+    snake.isCollide = false;
+    snake.getFood = false
+
+}
+
+
+
+main()
